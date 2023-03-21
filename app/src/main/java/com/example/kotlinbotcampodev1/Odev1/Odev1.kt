72042663fb11408ebc4237c23afb32248fd33e3e
ahmet enes <ahmetenestartici71@gmail.com>
2023-03-21 23:07:54 +0300

    odev1 ilk commit

---
+++ app/src/main/java/com/example/kotlinbotcampodev1/Odev1/Odev1.kt
@@ -0,0 +1,87 @@
+package com.example.kotlinbotcampodev1.Odev1
+
+import java.util.Date
+
+fun main() {
+
+    // val sonradan değiştirilemez olan tüm türdeki(int,string vs.) değerler için kullanılabilir.
+
+    val sehir="kahramanmaras"
+    val ulke="Turkiye"
+    val telefon="053551161"
+    val postaKodu="46150"
+    val email="denemekotlin@gmail.com"
+    val meslek="doktor"
+
+    //var, val gibi çalışır fakat buradaki değerler sonradan değiştirilebilirler.Tüm değerleri var değilde bazılarını val olarak kullanmamızın nedeni val ların bellekte daha az yer kaplamalarındandır.
+
+    var stokMiktari=10
+    var musteriAdi="Ahmet enes"
+    var bakiye=1000.895
+    var dogumGunu="21.05.2001"
+    var maas=100000
+
+    //    var değerAdi : Tür =Değeri     -> Burada yaptığımız işlem : solundaki türü sağa atıyoruz.
+
+    var medeniDurum : Boolean=true
+    var urunYorum : String="Gayet Memnun Kaldım"
+    var odemeTarihi : String="15.07.2008"
+    var odeme :Int=58973
+
+
+
+
+
+    var siparisAdeti=15
+
+    val arabaModeli="c3"
+    val kitapAdi="Hayvan Mezarligi"
+    val yayinlanmaTarihi="12.08.2009"
+    val indirimMiktari=20
+    val odaSayisi=5
+    val enlem="35 E 18"
+    val boylam="37 N 01"
+    val urunAdi="makas"
+
+    var yemekFiyati=35.99
+
+    val marka="LCW"
+    val muzikAdi="aşk yok olmaktır"
+    val videoSuresi=35.88
+    val urunPuani=4
+    val resimAdi="kaplumbaga terbiyecisi"
+    val dosyaFormati=".txt"
+    val renk="mor"
+    var renkKodu="242 242 242"
+    val telefonModeli="11S"
+    val ekranBoyutu="16.1"
+    val agirlik=369
+    val ulusalGun="29.10.1923"
+    val tatilGunu="cumartesi-pazar"
+
+    var rezervasyonTarihi="12.05.2024"
+
+    val sokakAdi="beyhekim"
+    val otobusHatti="H/14"
+    val kalanDakika=58
+    val takipKodu="1879"
+    val kuponSurewsi="13 gun"
+    val kuponKodu="98952"
+    val faturaAdresi="x mahallesi x sokak x adlı ev x.kat x.daire"
+
+
+
+
+
+println(sokakAdi)
+
+
+
+
+
+
+
+
+
+
+}
